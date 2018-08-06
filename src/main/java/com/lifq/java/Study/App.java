3689b0a6829eda50e175b73edfa4de648c6022fd
lifq <lifuqiang@xinfushe.com>
2018-08-06 13:48:05 +0800

    init

---
+++ src/main/java/com/lifq/java/Study/App.java
@@ -0,0 +1,13 @@
+package com.lifq.java.Study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
